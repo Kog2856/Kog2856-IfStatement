2ad04ddb5836d40ad0a174b08335508fb7e9a66e
Kog2856 Gaher <gaherkamal@gmail.com>
2023-03-06 18:36:36 +0000

    Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,13 +19,17 @@ public class Lab {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
-    public int bool(boolean bool, int x, int y){
-        return 0;
-        if (bool= true){
-            return x;
-        } else {
-            return y;
-        }
-        }
+     public int ifExample(boolean bool, int x, int y){
+       return bool == true ? x : y;
+     }
     }
-}
+
+    
+
+
+
+
+
+
+    
+
